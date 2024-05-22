106cb7fc5989acc150663823c41a948134c9b707
fokcin01 <bogdan090107@gmai.com>
2024-05-22 15:47:31 +0200

    Constants

--- src/main/java/client/to/Constants.java
+++ src/main/java/client/to/Constants.java
@@ -3,5 +3,6 @@ package client.to;
 public class Constants {
     public static final String LOGIN_OK = "login is ok";
     public static final String LOGIN_FAILED = "login is failed";
-
+    public static final String REGISTRATION_FAILED = "registration is failed";
+    public static final String REGISTRATION_OK = "registration is ok";
 }
